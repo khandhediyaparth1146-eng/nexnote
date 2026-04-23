10c6383861db5978f687f8c0fda9732a858efb22
parth khandhediya <khandhediyaparrth1146@gmail.com>
2026-04-23 11:33:43 +0530

    Build: Optimize chunks and increase size warning limit

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+        chunkSizeWarningLimit: 2000,
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        if (id.includes('lucide-react')) return 'icons';
+                        if (id.includes('framer-motion')) return 'animations';
+                        return 'vendor';
+                    }
+                }
+            }
+        }
+    }
 })
